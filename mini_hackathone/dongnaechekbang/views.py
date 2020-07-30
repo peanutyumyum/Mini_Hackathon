cb053f379472a13472e6df6e05d18475e3c17447
Chungking-express <rla1wlsdn@naver.com>
2020-07-30 16:10:39 +0900

    views오류해결

--- mini_hackathone/dongnaechekbang/views.py
+++ mini_hackathone/dongnaechekbang/views.py
@@ -7,9 +7,17 @@ def search(request):
 
 def result(request):
     bookstoreinfo = bookstore.objects.all()
-    query = request.GET.get('query','')
+    query = request.GET.get('query','') 
+    search_type = request.GET.get('type','')
     if query:
-        bookstoreinfo = bookstoreinfo.filter(Q(name__icontains=query)|Q(city_address_of_bookstore__icontains=query)|Q(trait__icontains=query)|Q(bookstore_information__icontains=query)).order_by('-time')
-
-    return render(request,'result.html',{'bookstoreinfo':bookstoreinfo,'query':query})
-# Create your views here.
+        if search_type == 'all':
+            bookstoreinfo = bookstoreinfo.filter(Q(name__icontains=query)| Q(city_address_of_bookstore__city__icontains=query) | Q(trait__traits__icontains=query)|Q(bookstore_information__icontains=query))
+        elif search_type == 'name':
+            bookstoreinfo = bookstoreinfo.filter(name__icontains=query)
+        elif search_type == 'city':
+            bookstoreinfo = bookstoreinfo.filter(city_address_of_bookstore__city__icontains=query)
+        elif search_type == 'trait':
+            bookstoreinfo = bookstoreinfo.filter(trait__traits__icontains=query)
+        elif search_type == 'info':
+            bookstoreinfo = bookstoreinfo.filter(bookstore_information__icontains=query)
+    return render(request, 'result.html',{'bookstoreinfo':bookstoreinfo , 'query':query,})
