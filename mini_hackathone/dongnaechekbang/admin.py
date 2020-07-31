c7836ddfe04cef2c77c775e12947698560abfc25
peanutyumyum <jrimit@gmail.com>
2020-07-31 21:16:41 +0900

    migration_problem_solved

--- mini_hackathone/dongnaechekbang/admin.py
+++ mini_hackathone/dongnaechekbang/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Bookstore, Trait, Evaluation_about_bookstore, Informations, Bookstore_event
 
 # Register your models here.
+
+admin.site.register(Trait),
+admin.site.register(Bookstore),
+admin.site.register(Informations),
+admin.site.register(Evaluation_about_bookstore),
+admin.site.register(Bookstore_event)
--- mini_hackathone/mini_hackathone/urls.py
+++ mini_hackathone/mini_hackathone/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from dongnaechekbang import views
+import dongnaechekbang.views
+
+app_name = 'dongnaechekbang'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('location/', views.location, name="location"),
+    path('location/<int:bookstore_id>', views.location_bookstore, name="bookstore")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
